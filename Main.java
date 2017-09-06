b1c6343edc975f1de2d685664be6427254809b07
thing91 <30793344+thing91@users.noreply.github.com>
2017-09-06 21:05:33 +0200

    added Main.java

---
+++ Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+	public static void main(String args[]){
+		Earth data = new Earth();
+		Mars new_data = new Mars();
+		// new_data.output();
+		// System.out.println(new_data.output());
+		// data.input();
+		new_data.calculation(data.input());
+	}
+}
